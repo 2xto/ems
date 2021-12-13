8737e717ec0e044f0500571096ec3eac9ae4e29b
Tomasz Tokarczyk <t.tokarczyk86@gmail.com>
2021-12-13 22:58:29 +0100

    generated project form start.spring.io

---
+++ src/test/java/com/github/xto/emsapp/EmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xto.emsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
