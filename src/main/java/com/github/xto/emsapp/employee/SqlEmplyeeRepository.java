3917299af7de5a9fc576f76136e78c25ff725693
Tomasz Tokarczyk <t.tokarczyk86@gmail.com>
2021-12-16 19:22:33 +0100

    correcting a typo

--- src/main/java/com/github/xto/emsapp/employee/SqlEmplyeeRepository.java
+++ src/main/java/com/github/xto/emsapp/employee/SqlEmplyeeRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 interface SqlEmployeeRepository extends EmployeeRepository, JpaRepository <Employee, Integer> {
     @Override
-    @Query(nativeQuery = true, value = "selec count(*) > 0 from employees where id=:id")
+    @Query(nativeQuery = true, value = "select count(*) > 0 from employees where id=:id")
     boolean existsById(@Param("id") Integer id);
 }
